a62972ad6efe9ffa7f6185275a84dc363a001632
christianrua <50930052+christianrua@users.noreply.github.com>
2020-05-16 23:07:48 -0500

    second example

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
 import React from "react";
-import ReacDOM from "react-dom";
+import ReactDOM from "react-dom";
 
-ReacDOM.render(
+ReactDOM.render(
   <div>
-    <h1>Hello React!!!</h1>
-    <p>what???</p>
+    <h1>this is a List</h1>
+    <ul>
+      <li>item 1</li>
+      <li>item 2</li>
+      <li>item 3</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
