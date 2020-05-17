aba7e8c893d7f9f610edea2b3094f697ffb51ac5
christianrua <50930052+christianrua@users.noreply.github.com>
2020-05-17 14:24:58 -0500

    React styling practice

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
+//Create a React app from scratch.
+//Show a single h1 that says "Good morning" if between midnight and 12PM.
+//or "Good Afternoon" if between 12PM and 6PM.
+//or "Good evening" if between 6PM and midnight.
+//Apply the "heading" style in the styles.css
+//Dynamically change the color of the h1 using inline css styles.
+//Morning = red, Afternoon = green, Night = blue.
+
 import React from "react";
 import ReactDOM from "react-dom";
 
+const date = new Date();
+const time = date.getHours();
+
 const customStyle = {
-  color: "red",
-  fontSize: "20px",
-  border: "1px solid black"
+  color: ""
 };
 
+var message = "";
+
+if (time >= 0 && time < 12) {
+  customStyle.color = "red";
+  message = "Good morning";
+} else if (time >= 12 && time < 18) {
+  customStyle.color = "green";
+  message = "Good afternoon";
+} else {
+  customStyle.color = "blue";
+  message = "Good evening";
+}
+
 ReactDOM.render(
-  <h1 style={customStyle}>Hello World!</h1>,
+  <h1 className="heading" style={customStyle}>
+    {message}
+  </h1>,
   document.getElementById("root")
 );
