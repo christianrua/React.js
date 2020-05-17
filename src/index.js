b6d9f3ca56d57bdc32e92443d8f168cd0a56725a
christianrua <50930052+christianrua@users.noreply.github.com>
2020-05-16 23:06:14 -0500

    first example

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReacDOM from "react-dom";
+
+ReacDOM.render(
+  <div>
+    <h1>Hello React!!!</h1>
+    <p>what???</p>
+  </div>,
+  document.getElementById("root")
+);
