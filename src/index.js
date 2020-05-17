fc1512fb4d741b5a2bf53cc1325cc3edaa1e944a
christianrua <50930052+christianrua@users.noreply.github.com>
2020-05-17 14:00:56 -0500

    creating inline custom style

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
-//Create a react app from scratch.
-//It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
-
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 
-const companyName = "ACDEGAM_4.0";
-const year_development = 2020;
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
 
-ReactDom.render(
-  <div>
-    <p>Develop by {companyName}</p>
-    <p>Copyrigth {year_development}</p>
-  </div>,
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
   document.getElementById("root")
 );
