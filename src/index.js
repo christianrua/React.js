66ad92d44a5c26c30cc781e01cfad2a653e64bf5
christianrua <50930052+christianrua@users.noreply.github.com>
2020-05-16 23:29:31 -0500

    4. example

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+//Create a react app from scratch.
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDom from "react-dom";
 
-const firstName = "Christian";
-const lastName = "Rua";
-const luckyNumber = 7;
+const companyName = "ACDEGAM_4.0";
+const year_development = 2020;
 
-ReactDOM.render(
+ReactDom.render(
   <div>
-    <h1>
-      Hello {`${firstName} ${lastName}, whenever you type, thats appears`}!
-    </h1>
-    <p>Your lucky number is {luckyNumber}</p>
+    <p>Develop by {companyName}</p>
+    <p>Copyrigth {year_development}</p>
   </div>,
   document.getElementById("root")
 );
