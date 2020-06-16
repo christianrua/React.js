8c5dd1370d96f85ce8c2a1eb41235ba835741bcc
Christian Rua <christian.rua@gmail.com>
2020-06-15 20:40:23 -0500

    destructuring and event handling practice

---
+++ event-handling-in-react/src/components/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleOnClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onClick={handleOnClick}
+      >
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default App;
